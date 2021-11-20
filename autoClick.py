6e1a1f0982bfe5766228137d32454c74e330b642
NutCrazypanda <56244402+NutCrazypanda@users.noreply.github.com>
2021-11-20 13:57:35 +0700

    Create autoClick.py

---
+++ autoClick.py
@@ -0,0 +1,79 @@
+import sys
+import pyautogui
+from pynput.keyboard import Key, Listener
+import threading
+
+
+
+thread_list =[]
+
+def clicker(numberOfIterations, coords):
+    while True:
+        for _ in range(numberOfIterations):
+            for i in coords:
+                x = i[0]
+                y = i[1]
+                n = i[2]
+                t = i[3]
+
+                for __ in range(int(n)):
+                    pyautogui.moveTo(int(x), int(y), duration=int(t))
+                    pyautogui.mouseDown()
+                    pyautogui.mouseUp()
+                    print("clicked (" + str(x) + "," + str(y) +")")
+
+
+def listen():
+    with Listener(
+            on_press=on_press,
+            on_release=on_release) as listener:
+        listener.join()
+
+
+def on_press(key):
+    #print('{0} pressed'.format(key))
+    pass
+
+
+def on_release(key):
+    #print('{0} release'.format(key))
+
+    if str(key) == "'s'":
+        print("Starting autoclick...")
+        thread = threading.Thread(target=clicker,args=( numberOfIterations ,coords, ) )
+        thread_list.append(thread)
+        thread.start()
+
+    if key == Key.esc:
+        # Stop listener
+        print("Stop")
+        return False
+
+
+numberOfIterations = 1
+coords = []
+
+if len(sys.argv) > 1:
+    if( sys.argv[1].isdigit() ):
+        numberOfIterations = sys.argv[1]
+    else:
+        print("Error!\nnumber of iterations must be a valid number!")
+        exit(0)
+
+try:
+    f = open("Coordinates.txt","r")
+
+    try:
+        for i in f.readlines():
+            coords.append(i.split())
+    except:
+        print("Error!\nCoordinates.txt file is empty!")
+        exit(0)
+
+except:
+    print("Error!\nCoordinates.txt file not found!")
+    exit(0)
+
+thread1 = threading.Thread(target=listen)
+thread_list.append(thread1)
+thread1.start()
